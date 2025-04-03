f3ced53697a9244fbe255c9c5017d0f1b4c2e915
ruth.deguzman_novare <ruth.deguzman_novare@MIS-1YPHLS3>
2025-04-03 20:48:48 +0800

    test suite

---
+++ Test Suites/test.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>test</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>919928cb-6cdb-4cae-8bda-3f870e91b5f4</testSuiteGuid>
+   <testCaseLink>
+      <guid>6a37566e-54ea-4391-9ff0-22b362a3d3fa</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TestGitrepo</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
